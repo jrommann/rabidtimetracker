c41e0cfaf704dafa0c5332979a65dde51821e48b
Jacob Rommann <jrommann@gmail.com>
2023-03-13 18:05:55 -0700

    adding database manager

---
+++ Rabid Time Tracker/DatabaseManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarfBuzzSharp;
+using SQLite;
+
+namespace Rabid_Time_Tracker
+{
+    internal class DatabaseManager
+    {
+        SQLiteConnection _db;
+        public DatabaseManager(string databasePath)
+        {
+            _db = new SQLiteConnection(databasePath);
+
+            _db.CreateTable<Session>();
+            _db.CreateTable<Period>();
+        }
+    }
+}
