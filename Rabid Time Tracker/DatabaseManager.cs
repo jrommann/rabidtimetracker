775398464da3cbf131f940514743f3861b4e4347
Jacob Rommann <jrommann@gmail.com>
2023-03-17 08:39:28 -0700

    added projects class and table

--- Rabid Time Tracker/DatabaseManager.cs
+++ Rabid Time Tracker/DatabaseManager.cs
@@ -10,11 +10,18 @@ namespace Rabid_Time_Tracker
 {
     internal class DatabaseManager
     {
+        static public DatabaseManager Instance { get; private set; }
+        static public void Create(string databasePath)
+        {
+            Instance = new DatabaseManager(databasePath);
+        }
+
         SQLiteConnection _db;
-        public DatabaseManager(string databasePath)
+        DatabaseManager(string databasePath)
         {
             _db = new SQLiteConnection(databasePath);
 
+            _db.CreateTable<Project>();
             _db.CreateTable<Session>();
             _db.CreateTable<Period>();
         }
---
+++ Rabid Time Tracker/Project.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabid_Time_Tracker
+{
+    [Table("Projects")]
+    internal class Project
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [Indexed]
+        public string Name { get; set; }        
+    }
+}
